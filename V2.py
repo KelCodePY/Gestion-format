bfda10e8adaffcbedf11f9ba3f363c9e2679ac73
KelCodePY <Kellyan.estarque.business@gmail.com>
2025-03-11 22:44:39 +0100

    Update V2.py

--- V2.py
+++ V2.py
@@ -6,7 +6,7 @@ from flask import Flask
 import threading
 
 # Configuration du bot
-API_TOKEN = "Ton token Telegram"  # Remplace par ton token Telegram
+API_TOKEN = "token_Telegram"  # Remplace par ton token Telegram
 bot = Bot(token=API_TOKEN)
 
 # Dispatcher et Router
